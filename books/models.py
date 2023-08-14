c8cddee862016b4adfa8a6066e1627ccb348a738
Dolera <Nanette.Dolera@Nabors.com>
2023-08-14 16:51:42 -0500

    Created Book Model for Book App + Registered Books & REST Framework in Project Settings Installed Apps Section

--- books/models.py
+++ books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    book_description = models.CharField(max_length=500)
