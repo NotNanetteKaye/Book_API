1f1cce6be65ce1e0c3c0495add6cace09514dc40
Dolera <Nanette.Dolera@Nabors.com>
2023-08-15 17:14:57 -0500

    Completed Delete Functionality for Book Views File

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -16,11 +17,17 @@ def books_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def book_detail(request, pk):
-    try:
-        book = Book.objects.get(pk=pk)
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'GET':
         serializer = BookSerializer(book)
         return Response(serializer.data)
-    except Book.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
